7f214a9ecafda2306612e5a0649d49723d116dde
re-mark-able <markbmaximus@gmail.com>
2026-07-02 12:01:10 +0800

    add presence

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -1,4 +1,4 @@
-const { Events } = require(`discord.js`);
+const { Events, ActivityType } = require(`discord.js`);
 const logger = require(`../utils/logger.js`);
 const { connectToDatabase } = require(`../utils/database.js`);
 
@@ -7,6 +7,14 @@ module.exports = {
   once: true,
   async execute(client) {
     await connectToDatabase();
+    client.user.setPresence({
+      activities: [
+        {
+          type: ActivityType.Custom,
+          name: `Doin' stuff`,
+        },
+      ],
+    });
     logger.info(`** Ready! Logged in as ${client.user.username}`);
   },
 };
