6d0065e0003e0fde55d5cadd287b7bd051cd31d5
re-mark-able <markbmaximus@gmail.com>
2026-07-02 14:20:52 +0800

    test

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -10,32 +10,53 @@ module.exports = {
     )
       return;
 
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
+    if (interaction.isChatInputCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
-      logger.error(`No command matching ${interaction.commandName} was found.`);
-      return;
-    }
-
-    try {
-      logger.info(
-        `[${interaction.user.username}] Executed [/${interaction.commandName}]`,
-      );
-      await command.execute(interaction);
-    } catch (error) {
-      logger.error(error);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({
-          content: "There was an error while executing this command!",
-          flags: MessageFlags.Ephemeral,
-        });
-      } else {
-        await interaction.reply({
-          content: "There was an error while executing this command!",
-          flags: MessageFlags.Ephemeral,
-        });
+      if (!command) {
+        logger.error(
+          `No command matching ${interaction.commandName} was found.`,
+        );
+        return;
       }
+
+      try {
+        logger.info(
+          `[${interaction.user.username}] Executed [/${interaction.commandName}]`,
+        );
+        await command.execute(interaction);
+      } catch (error) {
+        logger.error(error);
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({
+            content: "There was an error while executing this command!",
+            flags: MessageFlags.Ephemeral,
+          });
+        } else {
+          await interaction.reply({
+            content: "There was an error while executing this command!",
+            flags: MessageFlags.Ephemeral,
+          });
+        }
+      }
+    } else if (interaction.isAutocomplete()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+
+      if (!command) {
+        logger.warn(
+          `No command matching ${interaction.commandName} was found for autocomplete.`,
+        );
+        return;
+      }
+
+      try {
+        await command.autocomplete(interaction);
+      } catch (error) {
+        logger.error(error, `Autocomplete error`);
+      }
+      return;
+    } else {
+      return;
     }
   },
 };
